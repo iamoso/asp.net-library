f4d468d973e863480c4255f0d66ac1c593b32491
Łukasz Rosik <lukiro96@gmail.com>
2018-05-01 14:34:13 +0200

    before changing properties to virtual

---
+++ Library/Controllers/BorrowedController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Models;
+using Library.Models.BorrowedViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Library.Controllers
+{
+    public class BorrowedController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly LibraryContext _context;
+
+        public BorrowedController(UserManager<ApplicationUser> userManager, LibraryContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new List<BorrowedViewModel>();
+            var borrowings = _context.Borrowings.ToList();
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            foreach (var borrowing in borrowings)
+            {
+                if (borrowing.ApplicationUser.Id == userId)
+                {
+                    var book = borrowing.Copy.Book;
+
+                    model.Add(new BorrowedViewModel
+                    {
+                        Borrowing = borrowing,
+                        Book = book
+                    });
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Library/Models/BorrowedViewModels/BorrowedViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.BorrowedViewModels
+{
+    public class BorrowedViewModel
+    {
+        public Borrowing Borrowing { get; set; }
+        public Book Book { get; set; }
+    }
+}
